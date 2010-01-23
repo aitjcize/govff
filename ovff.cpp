d377d1ea0677607430c702caec6269286199916f
AZ (Wei-Ning Huang) <aitjcize@gmail.com>
2010-01-24 06:43:19 +0800

    ovff: increase version number

--- ovff.cpp
+++ ovff.cpp
@@ -16,7 +16,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.  
  */
 
-#define VERSION "1.4.5"
+#define VERSION "1.5.0"
 
 #include "FileMg.h"
 #include "SQLite_Manage.h"
