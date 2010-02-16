eb2a638e2a7f0121e1707a6847dc5d8e560be599
AZ Huang <aitjcize@gmail.com>
2010-02-16 23:26:18 +0800

    govff: move some files to Misc

--- genpkg.sh
+++ genpkg.sh
@@ -5,6 +5,7 @@ version=`grep -m 1 VERSION src/resource.h | awk '{ print $3 }' | sed 's/"//g'`
 make clean
 cp -rf . ../govff-$version
 cd ../govff-$version
+mv Misc/* .
 make
 sudo checkinstall -D < input
 mv *.deb ../
